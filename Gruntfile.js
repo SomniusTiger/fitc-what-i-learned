b3e578290acd527f68f961204d361bbe0fd0c1e2
Eoin Thomas O'Hehir <eohehir@asheavenue.com>
2015-04-15 12:10:08 -0400

    Initial commit, taken from the little-things repo, plus Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            css: {
+                files: ['style/*.less'],
+                tasks: ['clean', 'less', 'cssmin', 'cachebreaker'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        clean: {
+            mincss: [
+                'sites/all/themes/aolrise/style/css/styles.min.css',
+                'sites/all/themes/aolrise/style/css/styles.css'
+            ]
+        },
+
+        less: {
+            development: {
+                options: {
+                    paths: ["style"]
+                },
+                files: {
+                    "style/styles.css": "style/styles.less"
+                }
+            }
+        },
+
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'style',
+                src: ['*.css', '!*.min.css'],
+                dest: 'style',
+                ext: '.min.css'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('cachebreaker', function() {
+        grunt.file.write('sites/all/themes/aolrise/latest_static_asset_key.txt', (new Date).getTime());
+    });
+
+    grunt.registerTask('default', ['clean', 'less', 'cssmin']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fitc-what-i-learned
-A slides.js presentation about what I learned at FITC 2015.
+# What I learned at FITC 2015
+A `slides.js` presentation about what I learned at FITC 2015. All styles are made with LESS and compiled via Grunt.
